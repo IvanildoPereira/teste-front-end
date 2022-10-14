af1022f47eb4b75d7450c6f58250d71d7cb152bb
Ivanildo Pereira <ivanildop99@gmail.com>
2022-10-14 20:41:02 -0300

    [backend] feat: add the  cors config to the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
 import express from "express";
+import cors from "cors";
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+
+// config cors
+app.use(cors({
+    origin: process.env.FRONEND_URL,
+    optionsSuccessStatus: 200,
+    credentials: true,
+}));
 
 // Passing the body to json
 app.use(express.json());
