ae340bdd538b45be8f6d297ec1ba045fe36f46c6
Ivanildo Pereira <ivanildop99@gmail.com>
2022-10-15 00:52:38 -0300

    [frontend] feat: search videos
    - search layout and styles
    - Check field if it's empty before call the fuction of the parent
    - Set animation to search skate in to top when the user click in search

---
+++ frontend/src/pages/home/components/Search.tsx
@@ -0,0 +1,77 @@
+import { Box, Button, FormHelperText, Input } from "@material-ui/core"
+import {SearchRounded} from "@material-ui/icons"
+import { useState } from "react"
+import styled, { keyframes } from "styled-components"
+import { isFieldFilled } from "../../../utils/validate"
+
+type searchProps = {
+    defaultValue: string;
+    placeholder: string,
+    onSearch: (text: string) => void,
+    isActiveAnimation: boolean
+}
+
+type animationProps = {
+    isActive: boolean;
+}
+
+const Search = ({ defaultValue, placeholder, onSearch, isActiveAnimation }: searchProps) => {
+    const [ searchText, setSearchText ] = useState<string>(defaultValue);
+    const [ errorSearchText, setErrorSearchText ] = useState<string | null>(null);
+
+    const handleSearch = () =>{
+        setErrorSearchText(null)
+        if(!isFieldFilled(searchText)){
+            setErrorSearchText("Please Fill this field, to get a list of videos!");
+            return;
+        } 
+
+        onSearch(searchText.trim());
+    }
+
+    return (
+        <CardSearch isActive = {isActiveAnimation}>
+            <Box style={{ display: 'flex', alignItems: 'center', width: "100%"}}>
+                <Input
+                    value={searchText}
+                    error = {errorSearchText ? true : false}
+                    placeholder={placeholder}
+                    onChange={(e)=> setSearchText(e.target.value)}
+                    style={{color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.1rem', flex: 1}}
+                    disableUnderline
+                />
+                <SearchButton color='primary' variant="contained" onClick={handleSearch}>
+                    <SearchRounded />
+                </SearchButton>
+            </Box>
+            {errorSearchText && <FormHelperText id="component-error-text">{errorSearchText}</FormHelperText>}
+        </CardSearch>
+    )
+}
+
+const skateInTop = keyframes`
+    0%{transform:translate(-50%,-50%)}
+    100%{transform:translate(-50%, -40vh)}
+`;
+
+const CardSearch = styled.div<animationProps>`
+    position: absolute;
+    top: 40vh;
+    left: 50%;
+    width: 100%;
+    transform: translate(-50%, -50%);
+    padding-left: 20px;
+    height: 65px;
+    background-color: #f5f5f5;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+    margin: 30px 0;
+    animation: ${props => props.isActive ? "" : skateInTop} ${skateInTop} 0.7s ease-in-out;
+    animation-fill-mode: forwards;
+
+`
+
+const SearchButton = styled(Button)`
+    height: 64px;
+`
+
+export default Search
