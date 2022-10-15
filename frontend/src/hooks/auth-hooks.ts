efe4c62cf38d41e0cc5953439e7bc6b0e0e0bf05
Ivanildo Pereira <ivanildop99@gmail.com>
2022-10-15 00:26:46 -0300

    [frontend] feat: add auth-hooks to handle session login
    - Login: email, username, and expirate date of the session
    - logout: clean session, userAuth, and expirate date
    - auto login: every reload page, the hooks will check with server to see if the client has a session open
    - auto logout: logout the client after the session time expired

---
+++ frontend/src/hooks/auth-hooks.ts
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import { useState, useCallback, useEffect } from 'react';
+import UserAuth from '../types/UserAuth';
+
+let logoutTimer: NodeJS.Timeout;
+
+export const useAuth = () => {
+  const [user, setUser] = useState<UserAuth | null>(null);
+  const [expirationDate, setExpirationDate] = useState<Date | null>(null)
+  const [isLoading, setLoading] = useState(true)
+
+  // Login
+  const login = useCallback((user: UserAuth, expirateDate: Date) => {
+    setExpirationDate(expirateDate);
+    setUser(user);
+    setLoading(false);
+  }, []);
+
+  // Logout
+  const logout = useCallback(async() => {
+    await axios("http://localhost:5000/users/logout", {
+      withCredentials: true
+    })
+    setUser(null);
+    setExpirationDate(null)
+  }, []);
+
+  // Auto-Logout when sessin expireted
+  useEffect(() => {
+    if (user && expirationDate) {
+      const remainingTime = expirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [user, logout, expirationDate]);
+
+  // Auto Login
+  useEffect(() =>{
+    const getUser = async() =>{
+      const response = await axios.get("http://localhost:5000/users/login",{
+        withCredentials: true
+      });
+      
+      if(response.data.user){
+        let {name, email} = response.data.user
+        let user = new UserAuth(name, email);
+        let expiresAt  = new Date(response.data.expiresAt);
+        login(user, expiresAt);
+      };
+      
+      setLoading(false)
+    }
+
+    getUser();
+  }, [login]);
+
+  return { user, login, logout, isLoading };
+};
