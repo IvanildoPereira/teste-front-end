7cbef46a1ac2cfa63bcb4297eca19726f76e4a8e
Ivanildo Pereira <ivanildop99@gmail.com>
2022-10-15 01:02:54 -0300

    [frontend] feat: homePage with search and infinity scroll of videos
    - get all the components as search and video list and controlling the state of them

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,7 +1,62 @@
+import { useState, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom';
+import LoadingSpinner from '../../components/LoadingSpinner';
+import useYoutubeSearch from '../../hooks/useYoutubeSearch';
+import Search from './components/Search';
+import VideoList from './components/VideoList';
+
 const HomePage = () => {
+  const [query, setQuery] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+  const [isSearchAnimationActive, setIsSearchAnimationActive] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams({});
+
+  const {
+    videos,
+    nextPageToken,
+    loading,
+    error
+  } = useYoutubeSearch(query, pageNumber)
+
+  useEffect(() =>{
+    let searchQuery = searchParams.get('search');
+    if(searchQuery){
+      handleSearch(searchQuery);
+    }
+  }, [])
+
+  const handleChangePage = () =>{
+    setPageNumber(prevPageNumber => prevPageNumber + 1);
+  }
+
+  const handleSearch = (text: string) =>{
+    if(!isSearchAnimationActive){
+      setIsSearchAnimationActive(true);
+      setSearchParams({search: text});
+      setTimeout(() => setQuery(text), 1000);
+      return;
+    }
+
+    setSearchParams({search: text});
+    setQuery(text);
+  }
+
   return (
-    <div>
-      HomePage
+    <div style={{position: "relative"}}>
+      <Search 
+        placeholder='Search a video' 
+        defaultValue={searchParams.get('search') || ""} 
+        onSearch = {handleSearch} 
+        isActiveAnimation = {isSearchAnimationActive}/>
+      {videos &&
+        <VideoList 
+          videos={videos} 
+          loading = {loading} 
+          hasNext = {!!nextPageToken} 
+          changeToNextPageNumber = {handleChangePage}/>
+      }      
+      <div>{loading && <LoadingSpinner color='#3f51b5'/>}</div>
+      <div>{error && 'Error'}</div>
     </div>
   )
 }
