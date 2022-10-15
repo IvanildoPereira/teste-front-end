bc3dc092cf4b007ec1426117b54174f68dae0210
Ivanildo Pereira <ivanildop99@gmail.com>
2022-10-14 21:51:38 -0300

    [backend] fix: typescript not recognize the UserAuth and rename text response of route not founded
    - Typescript didn't recognize the UserAuth in express-session, so I put the module in the index, it will have to find a nicest soluction for this problem and refactor the code
    - Just correct the english grammar

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,16 @@ import session from "express-session";
 import * as dotenv from 'dotenv';
 import { userAuthRouter } from "./routes/userAuthRouter";
 import { videoRouter } from "./routes/videoRouter";
+import UserAuth from "./models/UserAuth";
 dotenv.config();
 
 const app = express();
+
+declare module 'express-session' {
+    interface SessionData {
+      user: UserAuth | null;
+    }
+}
 
 // config cors
 app.use(cors({
@@ -39,7 +46,7 @@ app.use("/video/", videoRouter);
 
 // Route doesn't exist
 app.use((req, res, next) => {
-    res.status(404).json("Requested route don't exist!")
+    res.status(404).json("Requested route doesn't exist!")
 });
 
 app.listen(process.env.PORT || 3000);
