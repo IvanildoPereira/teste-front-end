39fcd77a25a75cb85f1ac909864a096e69aa0cd8
Ivanildo Pereira <ivanildop99@gmail.com>
2022-10-15 00:43:37 -0300

    [frontend] feat: create login page
    - login form
    - check fields
    - post login to serve
    - update auth-context to loggedIn

--- frontend/src/pages/login/LoginPage.tsx
+++ frontend/src/pages/login/LoginPage.tsx
@@ -1,10 +1,95 @@
+import { Paper, Grid, TextField, Button, FormControl, FormHelperText } from '@material-ui/core';
+import styled from 'styled-components';
+import React, { useContext, useState } from 'react'
+import { AuthContext } from '../../context/auth-context';
+import { isEmailValid, isFieldFilled } from '../../utils/validate';
+import axios from 'axios';
+
 const LoginPage = () => {
-    return (
-      <div>
-        LoginPage
-      </div>
-    )
+  const [username, setUsername] = useState("");
+  const [usernameError, setUsernameError] = useState<string | null>(null);
+  const [emailError, setEmailError] = useState<string | null>(null)
+  const [email, setEmail] = useState("");
+  const auth = useContext(AuthContext); 
+
+  const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+    setUsernameError(null);
+    setEmailError(null);
+    if(!isFieldFilled(username)) setUsernameError("Name is required!");
+    else if(!isFieldFilled(email)) setEmailError("Email is required!");
+    else if(!isEmailValid(email)) setEmailError("Email Invalid!");
+    else saveSessionUser();
   }
-  
+
+  const saveSessionUser = async() =>{
+    const body = JSON.stringify({
+      name: username.trim(),
+      email: email.trim()
+    });
+
+    const response = await axios.post("http://localhost:5000/users/login", body, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      withCredentials: true
+    });
+
+    let expiresAt  = new Date(response.data.expiresAt);
+    auth.login({name: username, email}, expiresAt);
+  }
+
+
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
+    > 
+      <CustomPaper elevation={10}>
+        <form onSubmit={handleOnSubmit}>
+          <FormControl fullWidth>
+            <TextInput 
+              label='Username' 
+              placeholder='Enter username' 
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              error = {usernameError ? true : false}
+              fullWidth 
+              //required
+            />
+            {usernameError && <FormHelperText id="component-error-text">{usernameError}</FormHelperText>}
+          </FormControl>
+          <FormControl fullWidth>
+            <TextInput 
+              label='Email' 
+              placeholder='Enter your email' 
+              //type='email' 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              error = {emailError ? true : false}
+              fullWidth 
+              //required
+            />
+             {emailError && <FormHelperText id="component-error-text">{emailError}</FormHelperText>}
+          </FormControl>
+          <Button type='submit' color='primary' variant="contained" fullWidth>Sign in</Button>
+        </form>
+      </CustomPaper>
+    </Grid>
+  )
+};
+
+const CustomPaper = styled(Paper)`
+  padding: 40px;
+`
+
+const TextInput = styled(TextField)`
+  margin: 10px 0;
+`
+
 export default LoginPage;
-  
+
