0ca2698882df22cba7ff58222297d8f694c6d854
Ivanildo Pereira <ivanildop99@gmail.com>
2022-10-15 00:33:40 -0300

    [frontend] feat: creating routes to auth users (homepage, details, and auth page(login)
    - create routes and check if user has a session

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,50 @@
+import { Container } from '@material-ui/core';
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import LoadingSpinner from './components/LoadingSpinner';
+import { AuthContext } from './context/auth-context';
+import { useAuth } from './hooks/auth-hooks';
+import DetailsPage from './pages/details/DetailsPage';
+import HomePage from './pages/home/HomePage';
+import LoginPage from './pages/login/LoginPage';
+
 const App = () => {  
+  const { login, logout, user, isLoading } = useAuth();
+  let routes;
+
+  if(user){
+    routes = (
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/video/:id" element={<DetailsPage/>} />
+        <Route path="/*" element={<Navigate to = "/"/>} />
+      </Routes>
+    )
+  }else{
+    routes = (
+      <Routes>
+        <Route path="/" element={<LoginPage/>} />
+        <Route path="/*" element={<Navigate to = "/"/>} />
+      </Routes>
+    )
+  }
+
   return (
-    <div>App</div>
+    <AuthContext.Provider value={{ isLoggedIn: !!user, user, login: login, logout: logout}}>
+      <Router>
+        {user && 
+        <div>header</div>
+        }
+        <Container>
+          {isLoading && <LoadingSpinner/>}
+          {!isLoading && routes}
+        </Container>
+      </Router> 
+    </AuthContext.Provider>
   );
 }
 
