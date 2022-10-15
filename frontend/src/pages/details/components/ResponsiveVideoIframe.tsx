329c3466d942d055284623357cc4b1459e5cc139
Ivanildo Pereira <ivanildop99@gmail.com>
2022-10-15 01:08:01 -0300

    [frontend] feat: a responsive iframe components
    - an iframe responsive that respect the choosen aspect ratio

---
+++ frontend/src/pages/details/components/ResponsiveVideoIframe.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { aspect } from "../../../types/AspectRatio";
+
+type ResponsiveVideoIframeProps = {
+    aspectRatio?: aspect;
+    src?: string 
+}
+
+type IframeContainerProps = {
+    aspectRatioSelected: aspect
+}
+
+const ResponsiveVideoIframe = (props: ResponsiveVideoIframeProps) =>{
+    return (
+       <IframeContainer aspectRatioSelected = {props.aspectRatio || aspect["16:9"]}>
+         <iframe src={props.src} title="YouTube video player"/>
+       </IframeContainer>
+    )
+}
+
+export default ResponsiveVideoIframe;
+
+const IframeContainer = styled.div<IframeContainerProps>`
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    padding-top: ${props => props.aspectRatioSelected};
+    margin: 20px 0;
+
+    iframe{
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+    }
+`
