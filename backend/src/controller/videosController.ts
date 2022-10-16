e9f57617aa9fabaf9c8149cd3ec5117fd05125bc
Ivanildo Pereira <ivanildop99@gmail.com>
2022-10-15 21:08:37 -0300

    [backend] fix: handle errors of youtube api return

--- backend/src/controller/videosController.ts
+++ backend/src/controller/videosController.ts
@@ -50,28 +50,32 @@ const getVideos = async (req: Request, res: Response) => {
     if(nextPageToken){
         params.pageToken = nextPageToken as string;
     }
-
-    let response = await axios({
-        method: 'GET',
-        url: `https://www.googleapis.com/youtube/v3/search?part=id,snippet&key=${process.env.YOUTUBE_API_KEY}`,
-        params
-    });
-    if(response.data && response.data.items.length > 0){
-        response.data.items.map((item: any) => {
-            let video = new Video(
-                item.id.videoId,
-                item.snippet.thumbnails.high.url,
-                item.snippet.title,
-                item.snippet.description,
-
-            )
-            videosList.push(video);
-        })
-        res.json({videos: videosList, nextPageToken: response.data.nextPageToken})
+    try{
+        let response = await axios({
+            method: 'GET',
+            url: `https://www.googleapis.com/youtube/v3/search?part=id,snippet&key=${process.env.YOUTUBE_API_KEY}`,
+            params
+        });
+        if(response.data && response.data.items.length > 0){
+            response.data.items.map((item: any) => {
+                let video = new Video(
+                    item.id.videoId,
+                    item.snippet.thumbnails.high.url,
+                    item.snippet.title,
+                    item.snippet.description,
+    
+                )
+                if(video.id) videosList.push(video);
+            })
+            res.json({videos: videosList, nextPageToken: response.data.nextPageToken})
+        }
+        else{
+            res.json({error: "cant find any"})
+        }
+    }catch(err){
+        res.status(500).json({error: "It wasn't possible to connect with the youtube API, check the limit or the API KEY"})
     }
-    else{
-        res.json({error: "cant find any"})
-    }
+    
 }
 
 export {
